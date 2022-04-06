9e087a95a2a5b08ef58ef43a26abac7ac1bbaf38
Maxim <machsim26@gmail.com>
2022-04-06 23:24:39 +0500

    try to connect git pages

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import bridge from "@vkontakte/vk-bridge";
-import App from "./App";
+import App from "./src/App";
 
 // Init VK  Mini App
 bridge.send("VKWebAppInit")
 
 ReactDOM.render(<App />, document.getElementById("root"));
 if (process.env.NODE_ENV === "development") {
-  import("./eruda").then(({ default: eruda }) => {}); //runtime download
+  import("./src/eruda").then(({ default: eruda }) => {}); //runtime download
 }
